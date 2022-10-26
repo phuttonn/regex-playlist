3125c1b37549010c7bb40ee4f550cc4db7576e3d
phuttonn <prestonhutton93@gmail.com>
2022-10-26 12:47:20 -0600

    Add validation function

--- validation.js
+++ validation.js
@@ -5,9 +5,14 @@ const patterns = {
     telephone: /^\d{11}$/
 };
 
+// validation function
+function validate(field, regex){
+    console.log(regex.test(field.value));
+}
 
 inputs.forEach((input) => {
     input.addEventListener('keyup', (e) => {
-       console.log(e.target.attributes.name.value); 
-    })
-})
+       //console.log(e.target.attributes.name.value); 
+       validate(e.target, patterns[e.target.attributes.name.value])
+    });
+});
